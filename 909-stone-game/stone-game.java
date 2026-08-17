d3c4bc6687fa588243c0106a20dd2fe228f2d6f9
Aditya Patel <adityapatelatwork@gmail.com>
2026-08-17 14:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (33.55%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        Arrays.sort(piles);
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int n = piles.length;
-        for(int i= 0; i < n; i++){
-            if(i % 2 == 0) cnt1 += piles[i];
-            else cnt2 += piles[i];
-        }
+        // Arrays.sort(piles);
+        // int cnt1 = 0;
+        // int cnt2 = 0;
+        // int n = piles.length;
+        // for(int i= 0; i < n; i++){
+        //     if(i % 2 == 0) cnt1 += piles[i];
+        //     else cnt2 += piles[i];
+        // }
 
-        return cnt1 < cnt2;
+        // return cnt1 < cnt2;
+        return true;
     }
 }
