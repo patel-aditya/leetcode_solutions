eaa6478e190044b45b98b666d6237b390cb182d8
Aditya Patel <adityapatelatwork@gmail.com>
2026-08-26 12:41:54 +0530

    Time: 2 ms (40.94%) | Memory: 44 MB (58.39%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,18 +2,25 @@ class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         StringBuilder sb = new StringBuilder();
         int n = s.length();
-        List<String> list = new ArrayList<>();
+        // List<String> list = new ArrayList<>();
         int i = 0;
         int j = 0;
         int count = 0;
         int minLen = 500;
+        String res = s;
+        boolean isTrue = false;
         while (j < n) {
             char c = s.charAt(j);
             sb.append(c);
             if (c == '1')
                 count++;
             while (i <= j && count >= k) {
-                list.add(sb.toString());
+                isTrue = true;
+                if(sb.length() < res.length()) res = sb.toString();
+                else if(sb.length() == res.length()){
+                int compare = sb.toString().compareTo(res);
+                if(compare < 0) res = sb.toString();
+            }
                 minLen = Math.min(minLen, sb.length());
                 if (s.charAt(i) == '1')
                     count--;
@@ -23,17 +30,8 @@ class Solution {
             j++;
         }
 
-        if (list.size() == 0)
+        if (!isTrue)
             return "";
-
-        String res = s;
-        for(String str: list){
-            if(str.length() < res.length()) res = str;
-            else if(str.length() == res.length()){
-                int compare = str.compareTo(res);
-                if(compare < 0) res = str;
-            }
-        }
 
         return res;
     }
