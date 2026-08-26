503a0291ab683e578265e42f1473cc09397dcaa0
Aditya Patel <adityapatelatwork@gmail.com>
2026-08-26 12:04:54 +0530

    Time: 3 ms (22.82%) | Memory: 44.6 MB (10.07%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,22 +2,19 @@ class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         StringBuilder sb = new StringBuilder();
         int n = s.length();
-        TreeSet<String> set = new TreeSet<>((a, b) -> {
-            if (a.length() != b.length()) {
-                return a.length() - b.length();
-            }
-            return a.compareTo(b);
-        });
+        List<String> list = new ArrayList<>();
         int i = 0;
         int j = 0;
         int count = 0;
+        int minLen = 500;
         while (j < n) {
             char c = s.charAt(j);
             sb.append(c);
             if (c == '1')
                 count++;
             while (i <= j && count >= k) {
-                set.add(sb.toString());
+                list.add(sb.toString());
+                minLen = Math.min(minLen, sb.length());
                 if (s.charAt(i) == '1')
                     count--;
                 sb.deleteCharAt(0);
@@ -26,9 +23,18 @@ class Solution {
             j++;
         }
 
-        if (set.size() == 0)
+        if (list.size() == 0)
             return "";
 
-        return set.first();
+        String res = s;
+        for(String str: list){
+            if(str.length() < res.length()) res = str;
+            else if(str.length() == res.length()){
+                int compare = str.compareTo(res);
+                if(compare < 0) res = str;
+            }
+        }
+
+        return res;
     }
 }
