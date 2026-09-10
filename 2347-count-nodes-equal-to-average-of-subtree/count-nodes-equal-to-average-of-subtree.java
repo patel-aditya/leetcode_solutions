e4cee9e5da577968290a6e8c0ce5ca27f7c823b8
Aditya Patel <adityapatelatwork@gmail.com>
2026-09-10 19:01:30 +0530

    Time: 1 ms (56.32%) | Memory: 46.5 MB (7.46%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,37 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int res = 0;
-
-    public int[] solve(TreeNode node) {
-        if (node == null) {
-            return new int[] { 0, 0 };
+    int count = 0;
+    
+    Pair<Integer, Integer> postOrder(TreeNode root) {
+        if (root == null) {
+            return new Pair(0, 0);
         }
+        
+        // First iterate over left and right subtrees.
+        Pair<Integer, Integer> left = postOrder(root.left);
+        Pair<Integer, Integer> right = postOrder(root.right);
+        
+        int nodeSum = left.getKey() + right.getKey() + root.val;
+        int nodeCount = left.getValue() + right.getValue() + 1;
 
-        int[] left = solve(node.left);
-        int[] right = solve(node.right);
-
-        if ((left[1] + right[1] + node.val) / (left[0] + right[0] + 1) == node.val)
-            res++;
-        // System.out.println( sum +  " " + (1  + left + right));
-        return new int[] { 1 + left[0] + right[0], node.val + left[1] + right[1] };
+        // Check if the average of the subtree is equal to the node value.
+        if (root.val == nodeSum / (nodeCount)) {
+            count++;
+        }
+        
+        // Return the sum of nodes and the count in the subtree.
+        return new Pair(nodeSum, nodeCount);
     }
-
+    
     public int averageOfSubtree(TreeNode root) {
-        solve(root);
-        return res;
+        postOrder(root);
+        return count;
     }
 }
