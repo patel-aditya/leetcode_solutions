4a2c0acda4ab10938477ccd98404006569ee9305
Aditya Patel <adityapatelatwork@gmail.com>
2026-09-20 23:00:08 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (10.65%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,8 +1,10 @@
 class Solution {
     public int reverseDegree(String s) {
         int res = 0;
-        for(int i = 0; i< s.length(); i++){
-            res += (123 - s.charAt(i)) * (i+1);//123 - ascii of s.charAt(i) will give reverseOrder numbering of alphabet like 123 - 97 = 26 of 'a'(97 ascii value) 123 - 98 = 25 of 'b'(98 ascii value) 123 - 122 of 'z'(122 ascii value)
+        int i = 1;
+
+        for(char c: s.toCharArray()){
+            res += Math.abs(c-'a' - 26)* i++;
         }
         return res;
     }
